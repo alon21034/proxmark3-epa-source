6345be1edc72fc63cb25339cbd60d209e955c236
MD631 <md631@MD631teki-iMac.local>
2013-09-01 14:38:14 +0800

    try to print nt

--- armsrc/mifareutil.c
+++ armsrc/mifareutil.c
@@ -195,6 +195,7 @@ int mifare_classic_authex(struct Crypto1State *pcs, uint32_t uid, uint8_t blockN
 		
 	// Skip 32 bits in pseudo random generator
 	nt = prng_successor(nt,32);
+	Dbprintf("nt = %x", nt);
 
 	//  ar+parity
 	for (pos = 4; pos < 8; pos++)
